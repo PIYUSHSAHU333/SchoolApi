b74350fe76bc907ba911876867ff3d6153f8c9b8
PiyushSahu <piyushsahuys@gmail.com>
2025-05-23 15:06:21 +0530

    Database fixes

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
 
